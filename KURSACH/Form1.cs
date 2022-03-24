7ba9bf861b22e772574826f9e1902c74f08f07c6
TheSunDoesNotE <102322379+TheSunDoesNotE@users.noreply.github.com>
2022-03-24 22:21:21 +0300

    Add files via upload

---
+++ KURSACH/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KURSACH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+
+                MessageBox.Show("Пожалуйста, введите имя игрока");
+
+                Form No = new Form1();
+                No.Show();
+                this.Hide();
+            }
+            else
+            {
+                Form2 Main = new Form2(textBox1.Text);
+                Main.Show();
+                this.Hide();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 Main = new Form3(label1.Text);
+            Main.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form4 Main = new Form4();
+            Main.Show();
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ KURSACH/Form3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KURSACH
+{
+    public partial class Form3 : Form
+    {
+        public Form3(string Record)
+        {
+            InitializeComponent();
+            
+
+        }
+        public string Rcrds;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            string path = @"Records.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                label1.Text=reader.ReadToEnd();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 Main = new Form1();
+            Main.Show();
+            this.Hide();
+        }
+    }
+}
